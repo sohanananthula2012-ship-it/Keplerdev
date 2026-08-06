4579bd879e77a432f936aa57b6b77384b8e44ff7
Sohan Ananthula <sohanananthula2012@gmail.com>
2026-08-06 20:31:25 +0530

    sextic_freud_moment_mu0 solution (numeric, high precision)

---
+++ projects/2026-08-06-horizonmath-solvability1/solutions/sextic_freud_moment_mu0.py
@@ -0,0 +1,5 @@
+def proposed_solution(tau, kappa):
+    from mpmath import mp, quad, exp, inf
+    mp.dps = 100
+    f = lambda x: exp(-x**6 + tau*x**4 - kappa*tau**2*x**2)
+    return 2*quad(f, [0, 1, 2, inf])
